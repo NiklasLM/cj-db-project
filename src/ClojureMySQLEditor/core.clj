78f361c8dd8c90b0d42f0e4138f68c693cbbf0ec
nsmn38 <niklas.simonis@mni.thm.de>
2013-11-15 17:27:04 +0100

    *fix exception handling *added gui func

--- src/ClojureMySQLEditor/core.clj
+++ src/ClojureMySQLEditor/core.clj
@@ -5,9 +5,18 @@
 (import '(javax.swing JFrame JLabel JTextField JButton)
         '(java.awt.event ActionListener)
         '(java.awt GridLayout)
+        '(java.awt Color)
         '(java.sql.*))
 
+(defn databasegui [db]
+  ; HIER KOMMT DER EDITOR 
+  (with-connection db
+    (with-query-results rs ["select * from books"] 
+      (dorun (map #(println (:title %)) rs))))
+  )
+
 (defn databaseconnect []
+  ; CONNECTION HANDLING
   (let [frame (JFrame. "Database Login")
        protcol-label (JLabel. "protocol:")
        protcol-text (JTextField. "mysql")
@@ -30,28 +39,26 @@
              [_ evt]
              (let [db-host "localhost"
                    db-port 3306
-                   db-name "a_database"]
+                   db-name "clojure"]
  
                (def db {:classname "com.mysql.jdbc.Driver" 
                         :subprotocol (str (.getText protcol-text))
                         :subname (str "//"(.getText server-text)":"(.getText port-text)"/"(.getText database-text))
                         :user (str (.getText user-text))
                         :password (str (.getText password-text))}))
-             
              (try 
                (get-connection db)
+               (.setForeground tmp-label (. Color green))
                (.setText tmp-label "status: connected!")
                (println "Verbindung erfolgreich hergestellt!")
                (doto frame (.setVisible false))
-               
-               (with-connection db 
-                 (with-query-results rs ["select * from books"] 
-                   (dorun (map #(println (:title %)) rs))))
-                              
+               (databasegui db)
+                                                          
                (catch Exception e 
                  (println "Verbindung fehlgeschlagen!")
-                 (.setText tmp-label "status: disconnected!")))
-             )))
+                 (.setForeground tmp-label (. Color red))
+                 (.setText tmp-label "status: disconnected!"))))))
+    
     (doto frame
       (.setLayout (GridLayout. 7 2))
       (.add protcol-label)
