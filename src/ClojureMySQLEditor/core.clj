5f89e590386adb7f54de167675689d48d7955e02
nsmn38 <niklas.simonis@mni.thm.de>
2013-11-15 14:51:22 +0100

    added database connection

---
+++ src/ClojureMySQLEditor/core.clj
@@ -0,0 +1,60 @@
+(ns ClojureMySQLEditor.core)
+
+(import '(javax.swing JFrame JLabel JTextField JButton)
+        '(java.awt.event ActionListener)
+        '(java.awt GridLayout)
+        '(java.sql.*))
+
+(defn databaseconnect []
+  (let [frame (JFrame. "Database Login")
+       protcol-label (JLabel. "protocol:")
+       protcol-text (JTextField. "mysql")
+       server-label (JLabel. "server:")
+       server-text (JTextField. "localhost")
+       port-label (JLabel. "server-port:")
+       port-text (JTextField. "3306")
+       database-name (JLabel. "database name:")
+       database-text (JTextField. "clojure")
+       user-label (JLabel. "user:")
+       user-text (JTextField. "root")
+       password-label (JLabel. "password:")
+       password-text (JTextField. "")
+       connect-button (JButton. "Connect")
+       tmp-label (JLabel. "")]
+    (.addActionListener
+     connect-button
+     (reify ActionListener
+            (actionPerformed
+             [_ evt]
+             (let [db-host "localhost"
+                   db-port 3306
+                   db-name "a_database"]
+ 
+               (def db {:classname "com.mysql.jdbc.Driver" 
+                        :subprotocol (str (.getText protcol-text))
+                        :subname (str "//"(.getText server-text)":"(.getText port-text)"/"(.getText database-text))
+                        :user (str (.getText user-text))
+                        :password (str (.getText password-text))}))
+             
+             (with-connection db 
+               (with-query-results rs ["select * from books"] 
+                 (dorun (map #(println (:title %)) rs)))))))
+    (doto frame
+      (.setLayout (GridLayout. 7 2))
+      (.add protcol-label)
+      (.add protcol-text)
+      (.add server-label)
+      (.add server-text)
+      (.add port-label)
+      (.add port-text)
+      (.add database-name)
+      (.add database-text)
+      (.add user-label)
+      (.add user-text)
+      (.add password-label)
+      (.add password-text)
+      (.add connect-button)
+      (.add tmp-label)
+      (.setSize 300 250)
+      (.setVisible true))))
+(databaseconnect)
